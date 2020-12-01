11ac6a8c15ae2ab36f2fe28a234db83071929145
KoviGabi <74302200+KoviGabi@users.noreply.github.com>
2020-12-01 18:48:31 +0100

    Handler selector outsourcing

--- src/js/server.js
+++ src/js/server.js
@@ -1,6 +1,4 @@
-import {Json_Handler} from "./json_handler.js";
-import {Base64_Handler} from "./base64_handler.js";
-import {Unknown_Handler} from "./unknown_handler.js";
+import {HandlerSelector} from "./handler_selector.js";
 
 var net = require('net');
 var server = net.createServer();
@@ -18,31 +16,3 @@ server.on('connection', function(socket) {
         console.log("Error: " + e.message);
     });
 }).listen(9999);
-
-function HandlerSelector(){
-    if (DataHandler("JSON")){
-        if (DataHandler("base64")){
-            DataHandler("else");
-        }
-    }
-}
-function DataHandler(handler){
-    try {
-        switch (handler){
-            case "JSON":
-                Json_Handler(data);
-                break;
-            case "base64":
-                Base64_Handler(data);
-                break;
-            case "else":
-                Unknown_Handler(data, 1000);
-                break;
-            default:
-                break;
-        }
-        return false;
-    } catch (e){
-        return true;
-    }
-}
